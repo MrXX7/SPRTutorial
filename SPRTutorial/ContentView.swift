ba2a5457b1047b849b422a1d31a3e0a67bc410ab
Oncu <oohancan@gmail.com>
2022-06-02 21:04:48 +0300

    add enum choices

--- SPRTutorial/ContentView.swift
+++ SPRTutorial/ContentView.swift
@@ -7,10 +7,37 @@
 
 import SwiftUI
 
+enum Choices: String, CaseIterable {
+    case Scissors = "✂️", Paper = "📄", Rock = "🪨"
+}
+
 struct ContentView: View {
+    
+    @State var computerChoice = Choices.allCases.first!
+    @State var gameOutcome = ""
+    
+    @State var wins = 0
+    @State var round = 0
+    
+    @State var showAlert = false
+    @State var showComputerChoice = false
+    
+    
     var body: some View {
-        Text("Hello, world!")
-            .padding()
+        GeometryReader{ geo in
+            VStack {
+//            Computer
+            VStack {
+                if !showComputerChoice {
+                    Text("🤖")
+                        .font(.system(size: 100))
+                } else {
+                    Text(computerChoice.rawValue)
+                }
+            }
+//            Player
+            }
+        }
     }
 }
 
