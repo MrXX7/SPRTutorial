d395d08bb4a399d71ffba220d8a9f67fc3c0f910
Oncu <oohancan@gmail.com>
2022-06-02 20:51:23 +0300

    Initial Commit

---
+++ SPRTutorial/SPRTutorialApp.swift
@@ -0,0 +1,17 @@
+//
+//  SPRTutorialApp.swift
+//  SPRTutorial
+//
+//  Created by Oncu Ohancan on 2.06.2022.
+//
+
+import SwiftUI
+
+@main
+struct SPRTutorialApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
